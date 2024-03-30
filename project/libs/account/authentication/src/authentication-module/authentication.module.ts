3e8ad9c4edb2134a47589ca011a7631bff40b79e
Dimaggio <zodiak-d@yandex.ru>
2024-03-31 02:45:22 +0300

    Revert "Добавил `AuthenticationController` и `AuthenticationService`."
    This reverts commit 8098079125c18b876cd410df155d57288e0fddbb.

--- project/libs/account/authentication/src/authentication-module/authentication.module.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.module.ts
@@ -1,10 +1,4 @@
 import { Module } from '@nestjs/common';
 
-import { AuthenticationController } from './authentication.controller';
-import { AuthenticationService } from './authentication.service';
-
-@Module({
-  controllers: [AuthenticationController],
-  providers: [AuthenticationService]
-})
+@Module({})
 export class AuthenticationModule {}
