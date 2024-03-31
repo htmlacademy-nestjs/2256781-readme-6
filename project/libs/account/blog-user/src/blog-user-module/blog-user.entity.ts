320b8b50a8342e1564814d7749301128d481727d
Dimaggio <zodiak-d@yandex.ru>
2024-03-31 22:27:22 +0300

    Добавил метод `setPassword` в `BlogUserEntity`.
    Пароли пользователей не должны хранится в открытом виде. Поэтому при
    регистрации пользователя, необходимо получить хеш пароля.
    Библиотека `bcrypt` в этом поможет нам.
    
    Реализуем в классе `BlogUserEntity` метод для выполнения хеширования.
    Назовём его `setPassword`.
    
    Внутри метода получим соль с помощью функции `genSalt` из пакета
    `bcrypt`. Аргументом ей необходимо передать количество раундов.
    Зафиксируем это значение в одноимённой константе.
    
    Затем запишем в свойство `passwordHash` хеш пароля, полученный с
    помощью функции `hash`. Для получения хеша необходимо передать пароль в открытом виде и соль.

--- project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
+++ project/libs/account/blog-user/src/blog-user-module/blog-user.entity.ts
@@ -1,5 +1,9 @@
+import { genSalt, hash } from 'bcrypt';
+
 import { Entity } from '@project/shared/core';
 import { StorableEntity, AuthUser } from '@project/shared/core';
+
+import { SALT_ROUNDS } from './blog-user.constant';
 
 export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
   public avatar: string;
@@ -32,5 +36,11 @@ export class BlogUserEntity extends Entity implements StorableEntity<AuthUser> {
       login: this.login,
       passwordHash: this.passwordHash,
     }
+  }
+
+  public async setPassword(password: string): Promise<BlogUserEntity> {
+    const salt = await genSalt(SALT_ROUNDS);
+    this.passwordHash = await hash(password, salt);
+    return this;
   }
 }
