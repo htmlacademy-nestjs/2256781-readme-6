594f91332416b05a545ee9def536df9bee9a00ef
Dimaggio <zodiak-d@yandex.ru>
2024-04-04 20:48:52 +0300

    Добавил аннотации для OpenAPI
    Воспользуемся декораторами `ApiProperty`, `ApiResponse`, `ApiTags` и другими, чтобы документировать API.
    
    Обратит внимание, мы начинаем с контроллера и постепенно покрываем все DTO, которые участвуют в работе контроллера.
    
    После завершения действия, запустите проект и откройте в браузере маршрут `/spec`. Вы увидите спецификацию, сформированную в OpenAPI.

--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,6 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'User\'s avatar',
+    example: 'https://assets.htmlacademy.ru/previews/779/20230522_f0b2228f-150.jpg'
+  })
   public avatar?: string;
+
+  @ApiProperty({
+    description: 'User\'s unique address',
+    example: 'user@user.ru'
+  })
   public email: string;
+
+  @ApiProperty({
+    description: 'User\'s login name',
+    example: 'Keks Ivanov',
+  })
   public login: string;
+
+  @ApiProperty({
+    description: 'User\'s password',
+    example: '123456'
+  })
   public password: string;
 }
--- project/libs/account/authentication/src/dto/login-user.dto.ts
+++ project/libs/account/authentication/src/dto/login-user.dto.ts
@@ -1,4 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginUserDto {
+  @ApiProperty({
+    description: 'User\'s uniq email',
+    example: 'user@user.ru',
+  })
   public email: string;
+
+  @ApiProperty({
+    description: 'User\'s password',
+    example: '123456'
+  })
   public password: string;
 }
