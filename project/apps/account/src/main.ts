da21a3eb405b59d3b8c89f7937143f672ef624cb
Dimaggio <zodiak-d@yandex.ru>
2024-04-19 00:11:55 +0300

    Подключил `ValidationPipe` глобально.
    В Nest из коробки поставляется с несколькими готовыми пайпами.
    
    `ValidationPipe` — универсальный пайп для организации валидации. Он
    из коробки дружит с class-validator, поэтому достаточно подключить его
    глобально, чтобы он начал проверять все DTO, помеченные декораторами
    из библиотеки `class-validator`.
    
    Для регистрации `ValidationPipe` глобально, внесём изменения в функцию
    `bootstrap` в файле `main.ts`. Зарегистрировать пайп глобально позволяет метод `useGlobalPipes`.

--- project/apps/account/src/main.ts
+++ project/apps/account/src/main.ts
@@ -3,7 +3,7 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
@@ -24,6 +24,8 @@ async function bootstrap() {
 
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('spec', app, swaggerDocument);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
   const port = configService.get('application.port') || 3000;
