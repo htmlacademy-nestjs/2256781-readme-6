05969cbab6f02c547d2acefb41b52f9b40dbda7e
Dimaggio <zodiak-d@yandex.ru>
2024-05-09 22:44:41 +0300

    Добавил функцию `parseTime`
    Добавим вспомогательную функцию `parseTime`. С её помощью разберём строку, содержащую время жизни токена.
    
    Также добавим вспомогательную функцию `createJWTPayload`. Она упростит подготовку полезной нагрузки токена.

--- project/libs/shared/helpers/src/lib/common.ts
+++ project/libs/shared/helpers/src/lib/common.ts
@@ -5,6 +5,9 @@ export const getDate = () => {
   return dayjs().toISOString();
 }
 
+export type DateTimeUnit = 's' | 'h' | 'd' | 'm' | 'y';
+export type TimeAndUnit = { value: number; unit: DateTimeUnit };
+
 export function fillDto<T, V>(
   DtoClass: new () => T,
   plainObject: V,
@@ -34,4 +37,23 @@ export function getMongoConnectionString({ username, password, host, port, datab
 
 export function getRabbitMQConnectionString({user, password, host, port}): string {
   return `amqp://${user}:${password}@${host}:${port}`;
+}
+
+export function parseTime(time: string): TimeAndUnit {
+  const regex = /^(\d+)([shdmy])/;
+  const match = regex.exec(time);
+
+  if (!match) {
+    throw new Error(`[parseTime] Bad time string: ${time}`);
+  }
+
+  const [, valueRaw, unitRaw] = match;
+  const value = parseInt(valueRaw, 10);
+  const unit = unitRaw as DateTimeUnit;
+
+  if (isNaN(value)) {
+    throw new Error(`[parseTime] Can't parse value count. Result is NaN.`);
+  }
+
+  return { value, unit }
 }
