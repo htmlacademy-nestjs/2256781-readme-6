71e1e3f76b9db6fa22f025821ebdf0ceb392d08a
Dimaggio <zodiak-d@yandex.ru>
2024-05-09 22:41:37 +0300

    Добавил тип `RefreshTokenPayload`
    В полезной нагрузке Refresh Token потребуется дополнительная
    информация — идентификатор токена. По нему будем производить поиск
    токена в базе данных. Создадим новый интерфейс `RefreshTokenPayload`
    и унаследуем его от существующего `TokenPayload`.

--- project/libs/shared/core/src/index.ts
+++ project/libs/shared/core/src/index.ts
@@ -14,6 +14,8 @@ export { TokenPayload } from './lib/interfaces/token-payload.interface';
 export { StoredFile } from './lib/interfaces/stored-file.interface';
 export { RequestWithTokenPayload } from './lib/interfaces/request-with-token-payload.interface';
 export { Subscriber } from './lib/interfaces/subscriber.interface';
+export { JwtToken } from './lib/interfaces/jwt-token.interface';
+export { RefreshTokenPayload } from './lib/interfaces/refresh-token-payload.interface';
 
 export { TGenericPost } from './lib/types/post-pack.type';
 export { TPostStatusList } from './lib/types/post-status.type';
@@ -22,4 +24,3 @@ export { TPostContentList } from './lib/types/post-type.type';
 export { PostContent } from './lib/enums/post-type.enum';
 export { SortDirection } from './lib/enums/sort-direction.enum';
 export { RabbitRouting } from './lib/types/rabbit-routing.enum';
-export { JwtToken } from './lib/interfaces/jwt-token.interface';
