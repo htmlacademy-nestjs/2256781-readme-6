3573ec6c1cad65dc7461e52d66565892b350e5ed
Dimaggio <zodiak-d@yandex.ru>
2024-05-09 16:36:59 +0300

    Добавил рассылку email уведомлений
    Внедрим в `EmailSubscriberController` сервис `MailService`. В обработчике `create` добавим отправку уведомлений с помощью вызова метода `sendNotifyNewSubscriber`.

--- project/libs/notify/email-subsriber/src/email-subscriber.controller.ts
+++ project/libs/notify/email-subsriber/src/email-subscriber.controller.ts
@@ -4,13 +4,13 @@ import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 import { EmailSubscriberService } from './email-subscriber.service';
 import { CreateSubscriberDto } from './dto/create-subscriber.dto';
 import { RabbitRouting } from '@project/shared/core';
-
-
+import { MailService } from './mail-module/mail.service';
 
 @Controller()
 export class EmailSubscriberController {
   constructor(
     private readonly subscriberService: EmailSubscriberService,
+    private readonly mailService: MailService,
   ) {}
 
   @RabbitSubscribe({
@@ -20,5 +20,6 @@ export class EmailSubscriberController {
   })
   public async create(subscriber: CreateSubscriberDto) {
     this.subscriberService.addSubscriber(subscriber);
+    this.mailService.sendNotifyNewSubscriber(subscriber);
   }
 }
